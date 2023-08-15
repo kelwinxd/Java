239c5a6936d82338001d7590de92defe33c91631
kelwinxd <kelwin_esechiel@hotmail.com>
2023-08-14 21:55:05 -0300

    Initial commit

---
+++ Intro/Begin.java
@@ -0,0 +1,23 @@
+import java.util.Date;
+public class Begin{
+    public static void main(String[] args){
+        //Primitive Types
+        byte age = 25;
+        long euaPeople = 331_000_000L;
+        float price = 2.5F;
+        char letter = 'A';
+        boolean isEligible = false;
+         System.out.println("Eua population: " + euaPeople);
+        
+        
+        // Referece type
+         Date now = new Date();
+         //sout// snippet for System.out.println()
+        System.out.println(now);
+
+        
+       
+
+    }
+
+}
